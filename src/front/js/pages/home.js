265941689d1751e25f0fa872cedd03ceaecf13b2
andreshg <123838445+AndHoyosGarzon@users.noreply.github.com>
2024-07-18 17:25:27 +0000

    renderizando rutas en home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,7 +5,6 @@ import { Context } from "../store/appContext";
 import Card from "../component/card";
 
 export const Home = () => {
-
   const rutas = [
     {
       img: "https://images.unsplash.com/photo-1535479672101-8486af672be0?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -29,7 +28,6 @@ export const Home = () => {
       exigencia: "Moderada",
     },
   ];
-
 
   const [userData, setUserData] = useState();
 
