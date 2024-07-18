d99f21387a96c6712e75031ec32cbb08764d117e
andreshg <123838445+AndHoyosGarzon@users.noreply.github.com>
2024-07-18 22:32:17 +0000

    diseñando cards de rutas

--- src/front/js/component/Card/card.js
+++ src/front/js/component/Card/card.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { GiPathDistance } from "react-icons/gi";
 import { GiMountainClimbing } from "react-icons/gi";
+import style from "../Card/style.module.css";
 
 const Card = ({ img, nombre, ubicacion, longitud, exigencia }) => {
   return (
-    <div className="card mt-3" style={{ width: "350px" }}>
+    <div className={style.container_card}>
+      <div className={style.card_img}>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <div className={style.card_text_title}>
+          <p className="fw-bolder"> {nombre} </p>
+          <p>{ubicacion}</p>
+        </div>
+        <div className={style.card_text_footer}>
+          <p>{longitud}</p>
+          <p>{exigencia}</p>
+        </div>
+      </div>
+    </div>
+
+    /*  <div className="card mt-3" style={{ width: "350px" }}>
       <img src={img} className="card-img-top" />
       <div className="card-body">
         <h5 className="card-title fw-bolder">{nombre}</h5>
@@ -18,7 +35,7 @@ const Card = ({ img, nombre, ubicacion, longitud, exigencia }) => {
           </p>
         </div>
       </div>
-    </div>
+    </div> */
   );
 };
 
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -13,25 +13,26 @@ import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+  if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "")
+    return <BackendURL />;
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                    <Routes>
-                        <Route element={<Landing />} path="/" />
-                        <Route element={<Login />} path="/login" />
-                        <Route element={<Register />} path="/register" />
-                        <Route element={<Home />} path="/home" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <Routes>
+          <Route element={<Landing />} path="/" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<Register />} path="/register" />
+          <Route element={<Home />} path="/home" />
+          <Route element={<h1>Not found!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
