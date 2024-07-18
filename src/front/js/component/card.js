74bebdbe75d00f70e7ac2a1de66eb9800974d125
andreshg <123838445+AndHoyosGarzon@users.noreply.github.com>
2024-07-18 17:18:06 +0000

    renderizando rutas en home

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -1,17 +1,21 @@
-const Card = () => {
+import React from "react";
+import { GiPathDistance } from "react-icons/gi";
+import { GiMountainClimbing } from "react-icons/gi";
+
+const Card = ({ img, nombre, ubicacion, longitud, exigencia }) => {
   return (
-    <div className="container-fluid my-5">
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+    <div className="card mt-3" style={{ width: "350px" }}>
+      <img src={img} className="card-img-top" />
+      <div className="card-body">
+        <h5 className="card-title fw-bolder">{nombre}</h5>
+        <p className="card-text h6">{ubicacion}</p>
+        <div className="d-flex justify-content-between h6 mt-4">
+          <p className="fw-bolder">
+            <GiPathDistance /> {longitud}
           </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <p className="fw-bolder">
+            <GiMountainClimbing /> {exigencia}
+          </p>
         </div>
       </div>
     </div>
--- src/front/js/pages/landing.js
+++ src/front/js/pages/landing.js
@@ -6,34 +6,36 @@ import { GiMountains } from "react-icons/gi";
 export const Landing = () => {
   const navigate = useNavigate();
   return (
-    <div className="container-fluid  py-3 text-center">
-      <div className="d-flex justify-content-between">
-        <div className="col-4 mt-1">
-          <h3 className="text-start  fw-bolder">
-            <span className="me-2 h1">
-              <GiMountains />
-            </span>
-            SenderosApp
-          </h3>
-        </div>
+    <>
+      <div className="container-fluid  py-3 text-center">
+        <div className="d-flex justify-content-between">
+          <div className="col-4 mt-1">
+            <h3 className="text-start  fw-bolder">
+              <span className="me-2 h1">
+                <GiMountains />
+              </span>
+              SenderosApp
+            </h3>
+          </div>
 
-        <div className="col-4 text-end mt-3">
-          <button
-            type="button"
-            onClick={() => navigate("/login")}
-            className="btn btn-primary fw-bolder border-dark btn-sm me-2 rounded-pill"
-          >
-            Iniciar Sesión
-          </button>
-          <button
-            type="button"
-            onClick={() => navigate("/register")}
-            className="btn btn-success fw-bolder border-dark btn-sm rounded-pill"
-          >
-            Registrarse
-          </button>
+          <div className="col-4 text-end mt-3">
+            <button
+              type="button"
+              onClick={() => navigate("/login")}
+              className="btn btn-primary fw-bolder border-dark btn-sm me-2 rounded-pill"
+            >
+              Iniciar Sesión
+            </button>
+            <button
+              type="button"
+              onClick={() => navigate("/register")}
+              className="btn btn-success fw-bolder border-dark btn-sm rounded-pill"
+            >
+              Registrarse
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
