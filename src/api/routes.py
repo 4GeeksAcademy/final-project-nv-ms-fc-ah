fe39acb3489ee789e2e5bc1e45ac5cebd0288c20
matii17 <154836261+matii17@users.noreply.github.com>
2024-07-29 23:29:21 +0000

    pull

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash # Asegúrate de importar esta función
 import cloudinary.uploader  #cloudinary
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
