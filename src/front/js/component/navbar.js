96c5b060fe9e8f971bb48f9849b2dd8ef0b65c9f
Kakomanato <160876603+Kakomanato@users.noreply.github.com>
2024-08-13 23:53:32 +0000

    responsive navbar y grupos

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -19,54 +19,67 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="container d-flex justify-content-between align-items-center">
-        <h3 className="text-start fw-bolder">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <h3 className="navbar-brand fw-bolder">
           <span className="me-2 h1">
             <GiMountains />
           </span>
           SenderosApp
         </h3>
-        <ul className="navbar-nav d-flex flex-row mb-0">
-          <li className="nav-item me-3">
-            <Link to={"/home"} className="nav-link" href="#">
-              Inicio
-            </Link>
-          </li>
-          <li className="nav-item me-3">
-            <Link to={"/senderos"} className="nav-link" href="#">
-              Senderos
-            </Link>
-          </li>
-          <li className="nav-item me-3">
-            <Link to={"/mis-rutas"} className="nav-link" href="#">
-              Favoritos
-            </Link>
-          </li>
-          <li className="nav-item me-3">
-            <Link to={"/grupos"} className="nav-link" href="#">
-              Grupos
-            </Link>
-          </li>
-          <li className="nav-item me-3">
-            <Link to="/userprofile" className="nav-link" >
-              Perfil
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/">
-              <button className="btn btn-danger" onClick={handleLogout}>
-                Cerrar Sesión
-              </button>
-            </Link>
-          </li>
-        </ul>
-        {error && (
-          <div className="alert alert-danger" role="alert">
-            {error}
-          </div>
-        )}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto d-flex d-lg-flex flex-column flex-lg-row align-items-end align-items-lg-center">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Inicio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/senderos"} className="nav-link">
+                Senderos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/mis-rutas"} className="nav-link">
+                Favoritos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/grupos"} className="nav-link">
+                Grupos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/userprofile" className="nav-link">
+                Perfil
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/">
+                <button className="btn btn-danger" onClick={handleLogout}>
+                  Cerrar Sesión
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error}
+        </div>
+      )}
     </nav>
   );
 };
